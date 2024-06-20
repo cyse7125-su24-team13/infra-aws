e0626b549f7e7357d9da3299ae2a4e5df853d82a
Rahhul1309 <abbavaram.r@northeastern.edu>
2024-06-20 19:21:01 -0400

    fix : added the code

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-cluster1"
+  cluster_name    = "my-cluster2"
   cluster_version = "1.30"
 
   # EKS Addons
@@ -19,9 +19,15 @@ module "eks" {
     eks-pod-identity-agent = {
       most_recent = true
     }
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+    }
   }
   create_iam_role           = false
   iam_role_arn              = aws_iam_role.cluster_role.arn
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access = true
   vpc_id                    = aws_vpc.main.id
   subnet_ids                = aws_subnet.private[*].id
   control_plane_subnet_ids = aws_subnet.public[*].id
@@ -30,7 +36,7 @@ module "eks" {
   eks_managed_node_groups = {
     example = {
       ami_type                 = "AL2_x86_64"
-      instance_types           = ["t2.micro"]
+      instance_types           = ["c3.large"]
       create_iam_role          = true
       capacity_type            = "ON_DEMAND"
       iam_role_name            = "eks-managed-node-group-complete-example"
@@ -43,9 +49,9 @@ module "eks" {
         AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
         additional                         = aws_iam_policy.node_additional.arn
       }
-      min_size     = 2
-      max_size     = 5
-      desired_size = 2
+      min_size     = 3
+      max_size     = 6
+      desired_size = 3
     }
   }
 
@@ -69,4 +75,21 @@ resource "aws_iam_policy" "node_additional" {
   })
 
   tags = {}
+}
+module "ebs_csi_irsa_role" {
+  source                = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  attach_ebs_csi_policy = true
+  role_policy_arns      = {
+    "AmazonEKS_CSI_Driver" = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  }
+
+  role_name             = "ebs-csi-irsa-role"
+
+  oidc_providers = {
+    example = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+
+      }
+    }
 }
--- vpc.tf
+++ vpc.tf
@@ -6,22 +6,22 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name   = "ex-eks-mng"
+  name   = "ex-eks-mng1"
   region = "us-east-1"
 
-  vpc_cidr = "10.1.0.0/16"
+  vpc_cidr = "10.2.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   public_subnet_cidrs = [
-    "10.1.1.0/24",
-    "10.1.2.0/24",
-    "10.1.3.0/24"
+    "10.2.1.0/24",
+    "10.2.2.0/24",
+    "10.2.3.0/24"
   ]
 
   private_subnet_cidrs = [
-    "10.1.4.0/24",
-    "10.1.5.0/24",
-    "10.1.6.0/24"
+    "10.2.4.0/24",
+    "10.2.5.0/24",
+    "10.2.6.0/24"
   ]
 
   tags = {
@@ -126,4 +126,25 @@ resource "aws_route_table_association" "private" {
   count          = length(aws_subnet.private)
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+resource "aws_security_group" "eks_cluster" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster-sg"
+  }
 }
