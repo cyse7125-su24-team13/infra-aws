3a7760319067aee374e21c8a3bffc4f1c7802415
Rahhul1309 <abbavaram.r@northeastern.edu>
2024-06-27 17:49:42 -0400

    added the code

--- kafka-postgresql.tf
+++ kafka-postgresql.tf
@@ -1,96 +1,3 @@
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
-}
-
-resource "helm_release" "kafka" {
-  depends_on = [module.eks]
-
-  name       = "kafka"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "kafka"
-  namespace  = "default"
-  version    = "25.2.0"
-
-  set {
-    name  = "listeners.client.protocol"
-    value = "PLAINTEXT"
-  }
-
-  set {
-    name  = "listeners.controller.protocol"
-    value = "PLAINTEXT"
-  }
-
-  set {
-    name  = "listeners.interbroker.protocol"
-    value = "PLAINTEXT"
-  }
-
-  set {
-    name  = "listeners.external.protocol"
-    value = "PLAINTEXT"
-  }
-
-  set {
-    name  = "controller.replicaCount"
-    value = "0"
-  }
-
-  set {
-    name  = "broker.replicaCount"
-    value = "3"
-  }
-
-  set {
-    name  = "broker.persistence.size"
-    value = "4Gi"
-  }
-
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-
-  set {
-    name  = "provisioning.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "provisioning.numPartitions"
-    value = "3"
-  }
-
-  set {
-    name  = "provisioning.replicationFactor"
-    value = "1"
-  }
-
-  set {
-    name  = "provisioning.topics[0].name"
-    value = "cve"
-  }
-
-  set {
-    name  = "kraft.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "zookeeper.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "zookeeper.persistence.size"
-    value = "2Gi"
-  }
-}
-
 resource "helm_release" "postgresql_ha" {
   depends_on = [module.eks]
 
@@ -144,10 +51,29 @@ resource "helm_release" "postgresql_ha" {
     name  = "pgpool.customUsers.passwords"
     value = "changeme"
   }
+
+  set {
+    name  = "pgpool.numInitChildren"
+    value = "32"
+  }
+
+  set {
+    name  = "pgpool.maxPool"
+    value = "100"
+  }
+
+  set {
+    name  = "pgpool.childLifeTime"
+    value = "300"
+  }
+
+  set {
+    name  = "pgpool.connectionLifeTime"
+    value = "600"
+  }
+
+  set {
+    name  = "pgpool.clientIdleLimit"
+    value = "60"
+  }
 }
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
-
